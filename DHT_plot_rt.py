6575113b559bc44f50b08f6e839509f32e44b298
William Ledda <villy80@hotmail.it>
2019-11-07 21:33:42 +0100

    Improvement of first implementation
    - DataSet class created
    - Low pass filter using exponential smoothing
    - Plot improved
    - Cleanup

--- DHT_plot_rt.py
+++ DHT_plot_rt.py
@@ -4,28 +4,21 @@
 # https://learn.sparkfun.com/tutorials/graph-sensor-data-with-python-and-matplotlib/update-a-graph-in-real-time
 
 
-import argparse
-import logging
+#import argparse
+#import logging
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import Adafruit_DHT
 import datetime as dt
-
+from DataSet import DataSet
 
 fig = plt.figure()
 tPlot = fig.add_subplot(2, 1, 1)
 hPlot = fig.add_subplot(2, 1, 2)
-
-class DataPlot():
-    def __init__(self):
-        self.tx = []
-        self.ty = []
-        self.hx = []
-        self.hy = []
     
 # This function is called periodically from FuncAnimation
-def plot_update(i, dataPl):
+def plot_update(i, t, h):
     # Try to grab a sensor reading.  Use the read_retry method which will retry up
     # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
     humidity, temperature = Adafruit_DHT.read_retry(22, 4)
@@ -41,31 +34,42 @@ def plot_update(i, dataPl):
         print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
         # Add x and y to lists
         now = dt.datetime.now().strftime('%H:%M:%S')
-        dataPl.tx.append(now)
-       	dataPl.ty.append(temperature)
 
-        dataPl.hx.append(now)
-        dataPl.hy.append(humidity)
+        #append nre data
+        t.append(now, temperature)
+        h.append(now, humidity)
 
-        # Limit x and y lists to 20 items
-        dataPl.tx = dataPl.tx[-30:]
-        dataPl.ty = dataPl.ty[-30:]
-		
-        dataPl.hx = dataPl.hx[-30:]
-        dataPl.hy = dataPl.hy[-30:]
+        #Resize dataset
+        t.resize(30)
+        h.resize(30)
 
         # Draw x and y lists
         tPlot.clear()
-        tPlot.plot(dataPl.tx, dataPl.ty)
-		
+        tPlot.plot(t.x, t.y)
+
         hPlot.clear()
-        hPlot.plot(dataPl.hx, dataPl.hy)
+        hPlot.plot(h.x, h.y)
+
+        #https://stackoverflow.com/questions/19273040/rotating-axis-text-for-each-subplot
+        #Roatate x-axes of each plot to show date properly
+        for ax in fig.axes:
+            plt.sca(ax)
+            plt.xticks(rotation=45)
+
+        #Abjust the distance between sublots
+        fig.tight_layout()
     else:
         print('Failed to get reading. Try again!')
 
 try:
-    data = DataPlot()
-    ani = animation.FuncAnimation(fig, plot_update, fargs=({data}), interval=1000)
+    #data = DataPlot()
+
+    #Create two datasets, one for tempreture and one for humidity
+
+    temp = DataSet()
+    humidity = DataSet()
+    ani = animation.FuncAnimation(fig, plot_update, fargs=(temp, humidity), interval=2000)
+
     plt.show()
 
 except KeyboardInterrupt:
