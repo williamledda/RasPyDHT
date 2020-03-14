b5e3ec70423c9d19ca98ed36459230f699da674f
William Ledda <villy80@hotmail.it>
2020-03-14 16:35:05 +0100

    Added support for MQTT and LCD and improvements
    - Added LCD1602 display for showing the following information:
      * Current date and time
      * Temperature and Humidity
    - Added support for MQTT (disabled by default)
    - Flush stdout
    - Added empty __init__.py for packaging
    - Refactoring to adhere to coding style

---
+++ DHT_lcd1602.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import LCD1602
+import time
+import datetime as dt
+import threading
+
+
+class DHTLcd(threading.Thread):
+    def __init__(self, name=''):
+        super().__init__(name=name)
+
+    def setup(self):
+        LCD1602.init(0x27, 1)  # init(slave address, background light)
+        LCD1602.write(0, 0, 'Welcome MyDHTPi!')
+        LCD1602.write(1, 1, 'Room ' + self.name)
+        time.sleep(2)
+
+    def run(self):
+        self.setup()
+        now = dt.datetime.now()
+        # Wait till next full second
+        time.sleep((1e6 - now.microsecond) / 1e6)
+
+        while not getattr(threading.currentThread(), "stop", False):
+            now = dt.datetime.now()
+            humidity = getattr(threading.currentThread(), "humidity", None)
+            temperature = getattr(threading.currentThread(), "temperature", None)
+
+            if humidity is not None and temperature is not None:
+                line = '{0:0.1f} (C) - {1:3.0f} %'.format(temperature, humidity)
+            else:
+                line = 'XX.Y (C) - HHH %'
+
+            LCD1602.write(0, 0, now.strftime('%d/%m - %H:%M:%S'))
+            LCD1602.write(0, 1, line)
+
+            # Wait till next full second
+            time.sleep((1e6 - now.microsecond) / 1e6)
+
+        LCD1602.clear()
+        LCD1602.write(0, 0, "Sopping...")
+        LCD1602.write(0, 1, "Bye Bye!!")
+        time.sleep(2.0)
+
+    def destroy(self):
+        LCD1602.clear()
+        pass
+
+
+if __name__ == "__main__":
+    lcd = DHTLcd(name='Test')
+    try:
+        lcd.start()
+        while True:
+            pass
+    except KeyboardInterrupt:
+        lcd.stop = True
+        lcd.join()
+        lcd.destroy()
--- DHT_read.py
+++ DHT_read.py
@@ -4,38 +4,108 @@ import sys
 import time
 import argparse
 import Adafruit_DHT
+import paho.mqtt.client as mqtt
+
+from DHT_lcd1602 import DHTLcd
+
 
 # Parse command line parameters.
-
-sensor_type = {'11': Adafruit_DHT.DHT11,
-               '22': Adafruit_DHT.DHT22,
-               '2302': Adafruit_DHT.AM2302}
-
-def dataread(type, gpio):
-    humidity, temperature = Adafruit_DHT.read_retry(type, gpio)
-
-    if humidity is not None and temperature is not None:
-        print('{0:0.1f},{1:0.1f}'.format(temperature, humidity))
-
-def parseCommandLine():
+def parse_cmd_option():
     parser = argparse.ArgumentParser()
     parser.add_argument('--type', default='22', help='dht sensor type (11, 22, or 2302)')
     parser.add_argument('--gpio', type=int, default=4, help='GPIO port number')
-    parser.add_argument('--samples', type=int, default=1, help='number of samples to collect')
     parser.add_argument('--sleep', type=int, default=2, help='Delay (s) between two readings')
+    parser.add_argument('--broker', default=None, help='MQTT broker host')
+    parser.add_argument('--verbose', action="store_true", help='Log info into console')
+    parser.add_argument('--room', default='Room_1', help='Room name')
     return parser.parse_args()
 
 
+def publish_data(mqtt_client, temp, hum, client_id):
+    if mqtt_client.is_connected():
+        mqtt_client.publish('RasPyDHT/' + client_id + '/temperature', temp)
+        mqtt_client.publish('RasPyDHT/' + client_id + '/humidity', hum)
+
+
+def on_connect_cbk(client, userdata, flags, rc):
+    if rc == 0:
+        print("Client connected: " + str(client.is_connected()))
+    else:
+        print("Error connecting to broker: " + str(rc))
+
+
+def on_disconnect_cbk(client, flags, rc):
+    if rc == 0:
+        print("Client disconnected")
+    else:
+        print("Error when disconnecting from broker: " + str(rc))
+
+
+def mqtt_init(args, client):
+    client.on_connect = on_connect_cbk
+    client.on_disconnect = on_disconnect_cbk
+
+    # Connect and start network loop in case a MQTT broker is provided
+    if args.broker is not None:
+        try:
+            if args.verbose:
+                print('Connecting to broker: ' + args.broker)
+            client.connect(args.broker)
+            client.loop_start()  # Start mqtt network loop
+        except ConnectionRefusedError:
+            print('Connection refused to broker: ' + args.broker)
+
+
+def mqtt_stop(args, client):
+    if client.is_connected():
+        if args.verbose:
+            print('Disconnecting client')
+        client.disconnect()
+
+    if args.broker is not None:
+        client.loop_stop()
+
+
 if __name__ == "__main__":
-    args = parseCommandLine()
+    sensor_type = {'11': Adafruit_DHT.DHT11,
+                   '22': Adafruit_DHT.DHT22,
+                   '2302': Adafruit_DHT.AM2302}
+    args = parse_cmd_option()
     dht = sensor_type[args.type]
 
-    #Perform the first read
-    dataread(dht, args.gpio)
+    client_id = args.room.replace(' ', '_')
+    client = mqtt.Client(client_id)
 
-    #Performs other reads
-    for i in range(1, args.samples):
-        time.sleep(args.sleep)
-        dataread(dht, args.gpio)
+    # Create thread for handling LCD
+    lcd = DHTLcd(name=args.room)
 
-    sys.exit(1)
+    try:
+        lcd.start()
+
+        mqtt_init(args, client)
+
+        while True:
+            humidity, temperature = Adafruit_DHT.read_retry(dht, args.gpio)
+
+            if humidity is not None and temperature is not None:
+                if client.is_connected():
+                    publish_data(client, temperature, humidity, client_id)
+
+                lcd.humidity = humidity
+                lcd.temperature = temperature
+
+                if args.verbose:
+                    print('{0:0.1f},{1:0.1f}'.format(temperature, humidity))
+
+            sys.stdout.flush()
+            time.sleep(args.sleep)
+
+    except KeyboardInterrupt:
+        lcd.stop = True
+        lcd.join()
+        lcd.destroy()
+
+        mqtt_stop(args, client)
+
+        sys.stdout.write("\rComplete!            \n")
+        sys.exit(1)
